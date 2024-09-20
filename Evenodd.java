2b35f42f4eb6b1705a6026721c79deeef212cec8
tharak5 <tharaknath.maddipati@sasi.ac.in>
2024-09-20 16:03:08 +0530

    Create Evenodd.java

---
+++ Evenodd.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+class Evenodd
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int a = sc.nextInt();
+        if (a%2==0)  
+        System.out.println(a + " is even");
+        else
+        System.out.println(a + " is odd");
+    }
+}
