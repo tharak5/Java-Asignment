547341ae4c576315046a78aaf5b8df1de94f944d
tharak5 <tharaknath.maddipati@sasi.ac.in>
2024-09-27 11:49:14 +0530

    Create Dowhiledemo.java

---
+++ Dowhiledemo.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Dowhiledemo
+
+{
+
+public static void main(String args[])
+
+{
+
+int i=0;
+
+do
+
+{
+
+System.out.println("Hello World");
+
+i++;
+
+}
+
+while(i<3);
+
+}
+
+}
